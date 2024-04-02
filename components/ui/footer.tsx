f02b20f025623d3dad5338c42f3efa14e362bf34
ramya72002 <nsriramya7@gmail.com>
2024-04-02 18:29:03 +0530

    colour changed for borders

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
 
-          <div className="text-sm text-gray-600 mr-4">&copy;  All rights reserved.
+          <div className="text-sm text-gray-600 mr-4">&copy; Designed And Developed By Nvision It Team.
 
         </div>
 
